015d1cfd73db8fa1399010385d41d01712239a4d
egucciar <egucciar@gmail.com>
2014-07-07 10:14:02 -0400

    Removed old files

--- scalejs.autocomplete/config.js
+++
@@ -1,9 +0,0 @@
-var require = {
-    "paths":  {
-        "scalejs": "Scripts/scalejs-0.3.3",
-        "jQuery": "empty:",
-        "knockout": "empty:",
-        "jquery.better-autocomplete": "empty:",
-        "scalejs.mvvm": "empty:"
-    }
-};
--- scalejs.autocomplete/tools/uninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$project |
-	Remove-Paths 'scalejs.autocomplete' |
-	Remove-Shims 'jquery.better-autocomplete' |
-	Remove-ScalejsExtension 'scalejs.autocomplete' |
-	Out-Null
